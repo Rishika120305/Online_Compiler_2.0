b0d340f4e826452e05fcbfeaef4bf0d3e3805a16
Thoutam Rishika <2022csb1138@iitrpr.ac.in>
2024-07-28 16:32:23 +0530

    First Commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const { generateFile } = require('./generateFile');
+const { generateInputFile } = require('./generateInputFile');
 const { executeCpp } = require('./executeCpp');
-const cors  = require('cors');
+const cors = require('cors');
 
 //middlewares
 app.use(cors());
@@ -17,19 +18,20 @@ app.post("/run", async (req, res) => {
     // const language = req.body.language;
     // const code = req.body.code;
 
-    const { language = 'cpp', code } = req.body;
+    const { language = 'cpp', code, input } = req.body;
     if (code === undefined) {
         return res.status(404).json({ success: false, error: "Empty code!" });
     }
     try {
         const filePath = await generateFile(language, code);
-        const output = await executeCpp(filePath);
-        res.json({ filePath, output });
+        const inputPath = await generateInputFile(input);
+        const output = await executeCpp(filePath, inputPath);
+        res.json({ filePath, inputPath, output });
     } catch (error) {
         res.status(500).json({ error: error });
     }
 });
 
-app.listen(5000, () => {
-    console.log("Server is listening on port 5000!");
+app.listen(8000, () => {
+    console.log("Server is listening on port 8000!");
 });
